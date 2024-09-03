591c72d12659d38b0ad4a6e62e009307a9777535
Sneedjik <43564452+Sneedjik@users.noreply.github.com>
2024-09-04 03:35:23 +0600

    Update Main.java

--- src/main/java/com/sneedjik/Main.java
+++ src/main/java/com/sneedjik/Main.java
@@ -2,6 +2,6 @@ package com.sneedjik;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Automation!");
+        System.out.println("Hello Automation!!!!!""");
     }
 }
