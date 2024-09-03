ff6fd1559a4985d646dbce5eda3320de8a4aa611
Sneedjik <43564452+Sneedjik@users.noreply.github.com>
2024-09-04 04:05:49 +0600

    Update Main.java

--- src/main/java/com/sneedjik/Main.java
+++ src/main/java/com/sneedjik/Main.java
@@ -3,7 +3,7 @@ package com.sneedjik;
 private static String msg = ""Hello Automation!"";
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         System.out.println(msg);
     }
 }
